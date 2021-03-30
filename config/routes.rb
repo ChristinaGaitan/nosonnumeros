8df902dd52837ee48aa24312b3fc34783fe3075a
ChristinaGaitan <christina.gaitan@gmail.com>
2021-03-30 17:49:08 -0600

    Keep only needed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :totals
-  resources :pacientes
+  resources :totals, only: [:index, :show]
+  resources :pacientes, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
