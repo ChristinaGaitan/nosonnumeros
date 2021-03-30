a1cfaca375c31c18953c56f6674c9878dc195b88
ChristinaGaitan <christina.gaitan@gmail.com>
2021-03-30 17:34:44 -0600

    Schema for totals

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_29_225254) do
+ActiveRecord::Schema.define(version: 2021_03_30_220617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,6 +105,13 @@ ActiveRecord::Schema.define(version: 2021_03_29_225254) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["paciente_id"], name: "index_sintomas_on_paciente_id"
+  end
+
+  create_table "totals", force: :cascade do |t|
+    t.datetime "fecha"
+    t.integer "total"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
